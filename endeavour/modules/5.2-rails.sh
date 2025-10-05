c8296fd98d563da61358f55bb7a6d95508be5234
Rave <ezraravin@proton.me>
2025-10-06 02:16:19 +0700

    rails add noconfirm

--- endeavour/modules/5.2-rails.sh
+++ endeavour/modules/5.2-rails.sh
@@ -1,7 +1,7 @@
 echo "♦️ Ruby on Rails Setup"
 # Install essential build tools and dependencies
-sudo pacman -S base-devel libffi libyaml openssl zlib readline ncurses gmp
+sudo pacman -S --noconfirm base-devel libffi libyaml openssl zlib readline ncurses gmp
 # These are crucial for Ruby compilation
-sudo pacman -S libyaml openssl-1.1
+sudo pacman -S --noconfirm libyaml openssl-1.1
 curl https://mise.run | sh
 gem install rails --no-document
